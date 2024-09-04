77924f231dd00509767b417ecf750045865ba344
Dennn <dendysurya15@gmail.com>
2024-09-04 16:32:23 +0700

    testing send_log new console

--- startup_send_log_pengawasan_operator.py
+++ startup_send_log_pengawasan_operator.py
@@ -2,16 +2,28 @@ import subprocess
 import argparse
 import os
 import sys
+import time
 
 def run_send_log_script(script_dir):
     script_path = os.path.join(script_dir, "send_log_pengawasan_operator.py")
-    try:
-        subprocess.run([sys.executable, script_path,
-                        "--script_dir", script_dir,
-                        ], check=True)
-        print(f"Script '{script_path}' executed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running '{script_path}': {e}")
+    
+    while True:
+        try:
+            process = subprocess.Popen([sys.executable, script_path, "--script_dir", script_dir],
+                                       creationflags=subprocess.CREATE_NEW_CONSOLE)
+            process.wait()  # Wait for the process to complete
+
+            # Check the return code to see if the script ran successfully
+            if process.returncode == 0:
+                print(f"Script '{script_path}' executed successfully.")
+                break  # Exit loop if the script was successful
+            else:
+                print(f"Script '{script_path}' exited with error code {process.returncode}. Retrying...")
+
+        except Exception as e:
+            print(f"Error running '{script_path}': {e}")
+        
+        time.sleep(5)  # Wait before retrying
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set up the database and run the send_log_pengawasan_operator script.")
